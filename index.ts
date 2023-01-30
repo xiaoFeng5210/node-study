4a4bf17a2b1e2e1a343bdd3cc317d97095f47c63
Eadweard <804875728@qq.com>
2023-01-30 21:50:18 +0800

    dirname

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log('小风')
+import { readFile } from "fs";
+import { dirname, resolve } from 'path';
+
+const path = resolve(__dirname, 'corpus/data.json')
+
+readFile(path, { encoding: 'utf-8' }, (err, data) => {
+  if (!err) {
+    console.log(data)
+  } else {
+    console.log(err)
+  }
+})
+
+
