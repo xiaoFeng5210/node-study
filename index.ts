8829ae9634d138ef14b1cded17fc51411b2936a4
Eadweard <804875728@qq.com>
2023-01-30 22:53:19 +0800

    测试选取随机字符串

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
-import { readFile, readFileSync } from "fs";
-import { dirname, resolve } from 'path';
-import { randomPick } from './lib/random';
+import { readFileSync } from "fs";
+import { resolve } from 'path';
+import { createRandomPick } from './lib/random';
 
 const path = resolve(__dirname, 'corpus/data.json')
 
 const data = readFileSync(path, { encoding: 'utf-8' })
-console.log(data)
 
 const corpus = JSON.parse(data)
-const title = randomPick(corpus.title)
-
-const famous = randomPick(corpus.famous)
+const randomPickTitle = createRandomPick(corpus.title)
+const randomPickFamous = createRandomPick(corpus.famous)
+// const title = randomPickTitle()
+// const famous = randomPickFamous()
+for (let i = 0; i < 10; i++) {
+  console.log(randomPickFamous())
+}
 
 
 
--- lib/random.ts
+++ lib/random.ts
@@ -3,13 +3,6 @@ export function randomInt(min: number, max: number) {
   return Math.floor(min * (1 - p) + max * p);
 }
 
-export function randomPick(arr: string[]) {
-  const len = arr.length - 1
-  const index = randomInt(0, len);
-  [arr[index], arr[len]] = [arr[len], arr[index]]
-  return arr[index]
-}
-
 export function createRandomPick(arr: string[]) {
   arr = [...arr]
   function randomPick() {
