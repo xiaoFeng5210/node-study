8d81049e87137f75f4c37ebb00aea0257c19c08e
Eadweard <804875728@qq.com>
2023-01-31 23:36:19 +0800

    生成句子

--- index.ts
+++ index.ts
@@ -2,17 +2,35 @@ import { readFileSync } from "fs";
 import { resolve } from 'path';
 import { createRandomPicker } from './lib/random';
 
+type SentenceKey = 'famous' | 'bosh_before' | 'bosh' | 'said' | 'conclude';
+type Pick = () => string;
+type Replacer = {
+  [key in SentenceKey]?: Pick | string
+}
+
 const path = resolve(__dirname, 'corpus/data.json')
 
 const data = readFileSync(path, { encoding: 'utf-8' })
 
 const corpus = JSON.parse(data)
-const pickFamous = createRandomPicker(corpus.famous);
-const pickBosh = createRandomPicker(corpus.bosh);
 
-pickFamous(); // 随机取出一条名人名言
+function sentence(pick: Function, replacer: Replacer) {
+  let ret = pick()
+  for (const key in replacer) {
+    ret = ret.replace(new RegExp(`{{${key}}}`, 'g'),
+      typeof replacer[key as keyof Replacer] === 'function' ? (replacer[key as SentenceKey] as Pick)() : replacer[key as SentenceKey])
+  }
+  return ret
+}
 
-pickBosh(); // 随机取出一条废话
+const { famous, bosh_before, bosh, said, conclude } = corpus;
+const [pickFamous, pickBoshBefore, pickBosh, pickSaid, pickConclude] = [famous, bosh_before, bosh, said, conclude].map((item) => {
+  return createRandomPicker(item);
+});
+
+sentence(pickFamous, { said: pickSaid, conclude: pickConclude }); // 生成一条名人名言
+
+
 
 
 
