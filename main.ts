acc1b5fff1d6083b42c8b14c0f5564dbce04de4e
Eadweard <804875728@qq.com>
2023-09-25 14:54:24 +0800

    fs stream finish

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 import { egReadFile, egReadAllFiles } from './src/node/fs/index'
 import egEventEmitter from './src/node/events/index'
+import { pipeStream } from './src/node/stream/index'
 
 
 
 // egReadAllFiles()
-egEventEmitter()
+// egEventEmitter()
+pipeStream()
 
 
--- src/node/stream/index.ts
+++ src/node/stream/index.ts
@@ -2,6 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 const inputPath = path.resolve(__dirname, '../../assets/input.txt')
+const outputPath = path.resolve(__dirname, '../../assets/output.txt')
 
 const readerStream = fs.createReadStream(inputPath)
 let data = ''
@@ -19,4 +20,25 @@ readerStream.on('end', function () {
 readerStream.on('error', function (err) {
   console.log(err.stack)
 })
+
+function write() {
+  const writerStream = fs.createWriteStream(path.resolve(__dirname, '../../assets/output.txt'))
+  writerStream.write(data, 'utf8')
+  writerStream.end()
+
+  writerStream.on('finish', function() {
+    console.log('写入完成')
+  })
+
+  writerStream.on('error', function(err) {
+    console.log(err.stack)
+  })
+}
+
+export function pipeStream() {
+  const readerStream = fs.createReadStream(inputPath)
+  const writerStream = fs.createWriteStream(outputPath)
+
+  readerStream.pipe(writerStream)
+}
 
