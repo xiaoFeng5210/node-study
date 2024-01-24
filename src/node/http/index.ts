3412012fcdd165c7f1c748c926080860770fa30b
Eadweard <804875728@qq.com>
2024-01-24 13:53:48 +0800

    http

--- src/node/http/index.ts
+++ src/node/http/index.ts
@@ -1,6 +1,15 @@
 import http from 'node:http'
 import url from 'node:url'
 
-http.createServer((req, res) => {}).listen(520, () => {
+http.createServer((req, res) => {
+  const {pathname, query} = url.parse(req.url)
+  console.log(query, pathname)
+  if (req.method === 'GET') {
+
+  }
+  if (req.method === 'POST') {
+
+  }
+}).listen(520, () => {
   console.log('server is running at http://localhost:520')
 })
